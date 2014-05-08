35cffa7d9e9dea7b89b00b6cf41c30bd1752be7c
echanna <echanna@myseneca.ca>
2014-05-08 14:09:41 -0500

    adding files with git add *

---
+++ common/lib/xmodule/xmodule/vertical_module.py
@@ -0,0 +1,81 @@
+from xblock.fragment import Fragment
+from xmodule.x_module import XModule
+from xmodule.seq_module import SequenceDescriptor
+from xmodule.progress import Progress
+from pkg_resources import resource_string
+from copy import copy
+
+# HACK: This shouldn't be hard-coded to two types
+# OBSOLETE: This obsoletes 'type'
+class_priority = ['video', 'problem']
+
+
+class VerticalFields(object):
+    has_children = True
+
+
+class VerticalModule(VerticalFields, XModule):
+    ''' Layout module for laying out submodules vertically.'''
+
+    def student_view(self, context):
+        # When rendering a Studio preview, use a different template to support drag and drop.
+        if context and context.get('runtime_type', None) == 'studio':
+            return self.studio_preview_view(context)
+
+        return self.render_view(context, 'vert_module.html')
+
+    def studio_preview_view(self, context):
+        """
+        Renders the Studio preview view, which supports drag and drop.
+        """
+        return self.render_view(context, 'vert_module_studio_view.html')
+
+    def render_view(self, context, template_name):
+        """
+        Helper method for rendering student_view and the Studio version.
+        """
+        fragment = Fragment()
+        contents = []
+
+        child_context = {} if not context else copy(context)
+        child_context['child_of_vertical'] = True
+
+        for child in self.get_display_items():
+            rendered_child = child.render('student_view', child_context)
+            fragment.add_frag_resources(rendered_child)
+
+            contents.append({
+                'id': child.id,
+                'content': rendered_child.content
+            })
+
+        fragment.add_content(self.system.render_template(template_name, {
+            'items': contents,
+            'xblock_context': context,
+        }))
+        return fragment
+
+    def get_progress(self):
+        # TODO: Cache progress or children array?
+        children = self.get_children()
+        progresses = [child.get_progress() for child in children]
+        progress = reduce(Progress.add_counts, progresses, None)
+        return progress
+
+    def get_icon_class(self):
+        child_classes = set(child.get_icon_class() for child in self.get_children())
+        new_class = 'other'
+        for c in class_priority:
+            if c in child_classes:
+                new_class = c
+        return new_class
+
+
+class VerticalDescriptor(VerticalFields, SequenceDescriptor):
+    module_class = VerticalModule
+
+    js = {'coffee': [resource_string(__name__, 'js/src/vertical/edit.coffee')]}
+    js_module_name = "VerticalDescriptor"
+
+    # TODO (victor): Does this need its own definition_to_xml method?  Otherwise it looks
+    # like verticals will get exported as sequentials...
---
+++ common/test/acceptance/pages/studio/container.py
@@ -0,0 +1,118 @@
+"""
+Container page in Studio
+"""
+
+from bok_choy.page_object import PageObject
+from bok_choy.promise import Promise
+from . import BASE_URL
+
+from selenium.webdriver.common.action_chains import ActionChains
+
+class ContainerPage(PageObject):
+    """
+    Container page in Studio
+    """
+
+    def __init__(self, browser, unit_locator):
+        super(ContainerPage, self).__init__(browser)
+        self.unit_locator = unit_locator
+
+    @property
+    def url(self):
+        """URL to the container page for an xblock."""
+        return "{}/container/{}".format(BASE_URL, self.unit_locator)
+
+    def is_browser_on_page(self):
+
+        def _is_finished_loading():
+            # Wait until all components have been loaded
+            is_done = len(self.q(css=XBlockWrapper.BODY_SELECTOR).results) == len(
+                self.q(css='{} .xblock'.format(XBlockWrapper.BODY_SELECTOR)).results)
+            return (is_done, is_done)
+
+        # First make sure that an element with the view-container class is present on the page,
+        # and then wait to make sure that the xblocks are all there.
+        return (
+            self.q(css='body.view-container').present and
+            Promise(_is_finished_loading, 'Finished rendering the xblock wrappers.').fulfill()
+        )
+
+    @property
+    def xblocks(self):
+        """
+        Return a list of xblocks loaded on the container page.
+        """
+        return self.q(css=XBlockWrapper.BODY_SELECTOR).map(
+            lambda el: XBlockWrapper(self.browser, el.get_attribute('data-locator'))).results
+
+    def drag(self, source_index, target_index, after=True):
+        """
+        Gets the drag handle with index source_index (relative to the vertical layout of the page)
+        and drags it to the location of the drag handle with target_index.
+
+        This should drag the element with the source_index drag handle AFTER the
+        one with the target_index drag handle, unless 'after' is set to False.
+        """
+        draggables = self.q(css='.drag-handle')
+        source = draggables[source_index]
+        target = draggables[target_index]
+        action = ActionChains(self.browser)
+        action.click_and_hold(source).perform()  # pylint: disable=protected-access
+        action.move_to_element_with_offset(
+            target, 0, target.size['height'] / 2 if after else 0
+        ).perform()  # pylint: disable=protected-access
+        action.release().perform()
+
+
+class XBlockWrapper(PageObject):
+    """
+    A PageObject representing a wrapper around an XBlock child shown on the Studio container page.
+    """
+    url = None
+    BODY_SELECTOR = '.wrapper-xblock'
+    NAME_SELECTOR = '.header-details'
+
+    def __init__(self, browser, locator):
+        super(XBlockWrapper, self).__init__(browser)
+        self.locator = locator
+
+    def is_browser_on_page(self):
+        return self.q(css='{}[data-locator="{}"]'.format(self.BODY_SELECTOR, self.locator)).present
+
+    def _bounded_selector(self, selector):
+        """
+        Return `selector`, but limited to this particular `CourseOutlineChild` context
+        """
+        return '{}[data-locator="{}"] {}'.format(
+            self.BODY_SELECTOR,
+            self.locator,
+            selector
+        )
+
+    @property
+    def name(self):
+        titles = self.q(css=self._bounded_selector(self.NAME_SELECTOR)).text
+        if titles:
+            return titles[0]
+        else:
+            return None
+
+    @property
+    def children(self):
+        """
+        Will return any first-generation descendant xblocks of this xblock.
+        """
+        descendants = self.q(css=self._bounded_selector(self.BODY_SELECTOR)).map(
+            lambda el: XBlockWrapper(self.browser, el.get_attribute('data-locator'))).results
+
+        # Now remove any non-direct descendants.
+        grandkids = []
+        for descendant in descendants:
+            grandkids.extend(descendant.children)
+
+        grand_locators = [grandkid.locator for grandkid in grandkids]
+        return [descendant for descendant in descendants if not descendant.locator in grand_locators]
+
+    @property
+    def preview_selector(self):
+        return self._bounded_selector('.xblock-student_view')
